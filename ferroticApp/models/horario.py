abf4a7e18cc249d304e1e77a6ea6a8b3bca329ff
Henry David Nino Solano <85176198+HenryNino03@users.noreply.github.com>
2021-10-07 07:44:43 -0700

    Update horario.py

--- ferroticApp/models/horario.py
+++ ferroticApp/models/horario.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Horario(models.Model):
-    idHorario = models.AutoField(primary_key=True)
-    diainicio = models.DateTimeField()
-    diafin = models.DateTimeField(default=0)
-    fechainicio = models.DateTimeField()
-    fechafin = models.DateTimeField(default=True)
+    idHorario = models.BigAutoField(primary_key=True)
+    diaInicio = models.DateTimeField()
+    diaFin = models.DateTimeField(default=0)
+    fechaInicio = models.DateTimeField()
+    fechaFin = models.DateTimeField(default=True)
