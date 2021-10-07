7df8d2dc2f45ad652929ef5fe1aa0984ae244a26
Henry David Nino Solano <85176198+HenryNino03@users.noreply.github.com>
2021-10-07 08:06:30 -0700

    Update ubicacion.py
    actualizacion de atributos

--- ferroticApp/models/ubicacion.py
+++ ferroticApp/models/ubicacion.py
@@ -1,16 +1,14 @@
+
 from django.db import models
-from .pedido import idPedido
-from .usuario import idUsuario
+
 
 class Ubicacion(models.Model):
-    idHorario = models.AutoField(primary_key=True)
-    idPedido = models.ForeignKey(idPedido, related_name='Pedido', on_delete=models.CASCADE)
-    idUsuario=models.ForeignKey(idUsuario, related_name='Usuario', on_delete=models.CASCADE)
+    idUbicacion = models.BigAutoField(primary_key=True)
     departamento = models.IntegerField(default=0)
     ciudad = models.DateTimeField()
     direccion = models.BooleanField(default=True)
     barrio=models.CharField()
-    numeroinmueble=models.IntegerField()
-    instruccionesadicionales=models.CharField()
+    numeroInmueble=models.IntegerField()
+    instruccionesAdicionales=models.CharField()
     estado=models.CharField()
-    fechaactualizacion=models.DateTimeField()
+    fechaActualizacion=models.DateTimeField()
