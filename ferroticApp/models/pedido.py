3298347cb79bcd26a1c96fbe0450846a8f104f9c
Sebastian Rincon <sebasrs22@gmail.com>
2021-10-07 21:48:05 -0500

    MINT-22 Modelos y Serializers pedido

---
+++ ferroticApp/models/pedido.py
@@ -0,0 +1,18 @@
+from django.db import models
+from .usuario import Usuario
+from .ubicacion import Ubicacion
+
+
+class Pedido(models.Model):
+    idPedido = models.BigAutoField(primary_key=True)
+    usuarioComprador = models.ForeignKey(Usuario, related_name="compras", on_delete=models.CASCADE)
+    usuarioVendedor = models.ForeignKey(Usuario, related_name="ventas", on_delete=models.CASCADE )
+    fechaCompra = models.DateTimeField()
+    fechaEntrega = models.DateTimeField()
+    total = models.IntegerField()
+    ubicacion = models.ForeignKey(Ubicacion, related_name="pedidos", on_delete=models.CASCADE)
+    telefono = models.BigIntegerField()
+    nombreReceptor = models.CharField(max_length=70)
+    estado = models.CharField(max_length=30)
+    numeroArticulos = models.IntegerField()
+    observaciones = models.TextField()
---
+++ ferroticApp/serializers/__init__.py
@@ -0,0 +1,1 @@
+from .pedidoSerializer import PedidoSerializer
