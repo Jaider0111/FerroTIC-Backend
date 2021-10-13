e36ac55fb21e5425593a6b0ee85f79079c878cc9
Sebastian Rincon <sebasrs22@gmail.com>
2021-10-13 00:35:02 -0500

    Actualizacion comprar y crear

---
+++ ferroticApp/views/crearProductosView.py
@@ -0,0 +1,20 @@
+from rest_framework import status, generics
+from rest_framework.response import Response
+from rest_framework.serializers import Serializer
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenObtainSerializer
+from rest_framework.views import APIView
+from ferroticApp.models import Producto
+from rest_framework.permissions import IsAuthenticated
+
+
+from ferroticApp.serializers.productoSerializer import ProductoSerializer
+
+class CrearProductosView(generics.CreateAPIView):
+
+    queryset = Producto.objects.all()
+    serializer_class = ProductoSerializer
+    #permission_classes = (IsAuthenticated,)
+
+    def post(self,request,*args, **kwargs):
+
+        return super().post(request,*args, **kwargs)
--- ferroticProject/urls.py
+++ ferroticProject/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from ferroticApp.views import UsuarioLoginView, ProductosBusqueda, UsuarioRegistroView
+from ferroticApp.views import UsuarioLoginView, ProductosBusqueda, UsuarioRegistroView, comprarView
 from rest_framework_simplejwt.views import TokenRefreshView
+
+from ferroticApp.views.crearProductosView import CrearProductosView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,7 @@ urlpatterns = [
     path('refresh', TokenRefreshView.as_view()),
     path('registro', UsuarioRegistroView.as_view()),
     path('productos', ProductosBusqueda.as_view()),
+    path('comprar', comprarView.Comprar.as_view()),
+    path('producto/create', CrearProductosView.as_view())
+
 ]
