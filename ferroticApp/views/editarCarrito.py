2ec4e3def0d413083dbe671c1f0efb44f51473f3
javiersin25 <javiersin25@gmail.com>
2021-10-12 21:38:36 -0500

    deployment changes

--- ferroticApp/views/editarCarrito.py
+++ ferroticApp/views/editarCarrito.py
@@ -14,7 +14,7 @@ class EditarCarrito(views.APIView):
         
         serializer = ProductoPedidoSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            #serializer.save()
             return Response(serializer.data,status = status.HTTP_201_CREATED)
     
         return Response(serializer.data,status = status.HTTP_400_BAD_REQUEST)   
