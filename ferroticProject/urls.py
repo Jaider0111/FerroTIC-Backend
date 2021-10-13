b75b1913d5cd87283b1eb1aafc37a2c7ed3be6e1
javiersin25 <javiersin25@gmail.com>
2021-10-12 21:03:37 -0500

    deployment changes

--- ferroticProject/urls.py
+++ ferroticProject/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from ferroticApp.views import UsuarioLoginView, ProductosBusqueda, UsuarioRegistroView
+from ferroticApp.views import UsuarioLoginView, ProductosBusqueda, UsuarioRegistroView,EditarCarrito
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
@@ -9,4 +9,5 @@ urlpatterns = [
     path('refresh', TokenRefreshView.as_view()),
     path('registro', UsuarioRegistroView.as_view()),
     path('productos', ProductosBusqueda.as_view()),
+    path('carrito', EditarCarrito.as_view()),
 ]
