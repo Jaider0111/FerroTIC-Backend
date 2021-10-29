94e18ade927e543e366c66d823ab3c93badd0c52
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-10-29 17:17:57 -0500

    mas ajustes

--- ferroticApp/serializers/loginSerializer.py
+++ ferroticApp/serializers/loginSerializer.py
@@ -1,9 +1,24 @@
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from ferroticApp.models import Pedido, Usuario
 
 
 class LoginSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
         data = super().validate(attrs)
-        isAdmin = self.user.administrador
-        data['isAdmin'] = bool(isAdmin)
+        isAdmin = bool(self.user.administrador)
+        data['isAdmin'] = isAdmin
+        if not isAdmin:
+            pedido = None
+            try:
+                pedido = Pedido.objects.get(
+                    usuarioComprador__idUsuario=self.user.idUsuario, estado='CARRITO')
+            except:
+                pedido = Pedido(
+                    usuarioComprador=Usuario.objects.get(
+                        pk=self.user.idUsuario),
+                    usuarioVendedor=Usuario.objects.get(pk=12),
+                    estado='CARRITO',
+                )
+                pedido.save()
+            data['pedido'] = pedido.idPedido
         return data
