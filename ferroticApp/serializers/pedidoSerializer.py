25ed0e41d1f57d6885ba7abfee4a62537ae1586a
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-10-29 21:38:22 -0500

    ajustes

--- ferroticApp/serializers/pedidoSerializer.py
+++ ferroticApp/serializers/pedidoSerializer.py
@@ -1,15 +1,20 @@
-from ferroticApp.models import Pedido
+from ferroticApp.models import Pedido, Usuario
 from rest_framework import serializers
 
 
 class PedidoSerializer(serializers.ModelSerializer):
-    idUsuarioComprador = serializers.IntegerField(
-        source="usuarioComprador.idUsuario")
-    idUsuarioVendedor = serializers.IntegerField(
-        source="usuarioVendedor.idUsuario")
+    idUsuarioComprador = serializers.PrimaryKeyRelatedField(read_only=True,
+                                                            source="usuarioComprador.idUsuario")
+    idUsuarioVendedor = serializers.PrimaryKeyRelatedField(read_only=True,
+                                                           source="usuarioVendedor.idUsuario")
+    usuarioComprador = serializers.PrimaryKeyRelatedField(
+        write_only=True,
+        queryset=Usuario.objects.all())
+    usuarioVendedor = serializers.PrimaryKeyRelatedField(
+        write_only=True, queryset=Usuario.objects.all())
 
     class Meta:
         model = Pedido
         fields = ['idPedido', 'fechaCompra', 'fechaEntrega', 'total', 'ciudad', 'direccion', 'idUsuarioComprador', 'idUsuarioVendedor',
-                  'telefono', 'nombreReceptor', 'estado', 'numeroArticulos', 'observaciones']
+                  'telefono', 'nombreReceptor', 'estado', 'numeroArticulos', 'observaciones', 'usuarioComprador', 'usuarioVendedor']
         depth = 1
