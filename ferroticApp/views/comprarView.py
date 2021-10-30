b5ae7718d0972297fc7273dd2df308a10d9515d6
Jaider Andres Pinto Pinto <jaider.pinto@hotmail.com>
2021-10-29 20:56:03 -0500

    mas ajustes

--- ferroticApp/views/comprarView.py
+++ ferroticApp/views/comprarView.py
@@ -4,7 +4,7 @@ from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
-from ferroticApp.models import Pedido
+from ferroticApp.models import Pedido, Usuario
 
 from datetime import datetime
 
@@ -19,4 +19,12 @@ class ComprarView(views.APIView):
         pedido.estado = 'ENVIADA'
         pedido.fechaCompra = datetime.now()
         pedido.save()
-        return Response({'exitoso': True}, status.HTTP_200_OK)
+        pedidoN = None
+        pedidoN = Pedido(
+            usuarioComprador=Usuario.objects.get(
+                pk=pedido.usuarioComprador.idUsuario),
+            usuarioVendedor=Usuario.objects.get(pk=12),
+            estado='CARRITO',
+        )
+        pedidoN.save()
+        return Response({'exitoso': True, 'pedido': pedidoN.idPedido}, status.HTTP_200_OK)
